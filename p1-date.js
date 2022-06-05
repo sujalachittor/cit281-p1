9f8a2c18045b7d74b9386e33713f5f72841fcb1c
sujalachittor <82883879+sujalachittor@users.noreply.github.com>
2022-06-04 22:25:01 -0700

    Add files via upload

---
+++ p1-date.js
@@ -0,0 +1,5 @@
+/*
+    CIT 281 Project 1
+    Name: Sujala Chittor
+*/
+console.log((["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][(new Date()).getDay()]));
---
+++ p1-random.js
@@ -0,0 +1,33 @@
+/*
+    CIT 281 Project 1
+    Name: Sujala Chittor
+*/
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    //let min = 5;
+    //let max = 25;
+    let RandomInteger = Math.floor(Math.random() * (max - min) + min);
+    return RandomInteger
+}
+
+
+function generateRandomLetter() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    //let part = str.slice
+    //let i = alphabet.charAt(getRandomInteger(0, alphabet.length));
+    let randomstr = "";
+    let ln = getRandomInteger(5, 26);
+for (let i = 0; i < ln; i++ ){ 
+  randomstr = randomstr + alphabet.charAt(getRandomInteger(0, alphabet.length));
+  //console.log(randomstr);
+  } 
+  console.log(ln, " ",randomstr);
+}
+generateRandomLetter();
+
+  
+
+
+  
+
+
